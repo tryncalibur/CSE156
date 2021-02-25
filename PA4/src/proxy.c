0f719201ea89759f30351d691d9cd1966b564d3d
Trystan Nguyen <tryncalibur@gmail.com>
2021-02-25 02:52:45 -0800

    Get Finished

--- PA4/src/proxy.c
+++ PA4/src/proxy.c
@@ -14,6 +14,8 @@
 
 // Global Scope
 pthread_mutex_t GET_HOST;
+pthread_mutex_t WRITE_ENTRY;
+pthread_mutex_t SPRINTF_USE;
 
 // Tread Data Structure
 struct ThreadData{
@@ -29,13 +31,8 @@ struct ServerRes{
 
 // Helper Functions---------------------------------------------------------------------------------
 // Use to periodically check on socket
-int input_timeout (int fd, unsigned int secs){
-	fd_set set;
+int input_timeout (fd_set set, unsigned int secs){
 	struct timeval timeout;
-
-	FD_ZERO (&set);
-	FD_SET (fd, &set);
-
 	timeout.tv_sec = secs;
 	timeout.tv_usec = 0;
 	return(select(FD_SETSIZE, &set, NULL, NULL, &timeout));
@@ -55,7 +52,14 @@ int checkSites(char* fsite_name, char* siteName){
 	// Compare sites
 	char line[1024];
 	char* rere;
-	while(fgets(line, sizeof(line), forbiddenSites)){
+	while(fgets(line, sizeof(line), forbiddenSites) != NULL){
+		if (strstr(line, " ") != NULL){
+			fprintf(stderr, "Error: File does not follow correct format\n");
+			fclose(forbiddenSites);
+	    	return -1;
+		}
+
+		line[strlen(line)-2] = '\0';
 		rere = strstr(siteName, line);
 		if (rere != NULL) break;
 	}
@@ -74,9 +78,51 @@ struct hostent* gethostbyname_ts(char* name){
 	}
 	pthread_mutex_lock(&GET_HOST);
 	struct hostent* temp = gethostbyname(name);
+	if (temp == NULL){
+		free (rere);
+		pthread_mutex_unlock(&GET_HOST);
+    	return NULL;
+	}
 	*rere = *temp;
 	pthread_mutex_unlock(&GET_HOST);
 	return rere;
+}
+
+// Log client entry with time
+int logEntry(char* clientIP, char* request, char* host, int code, int size){
+	char rere[1024];
+	memset(rere, 0, sizeof(rere));
+
+	time_t now = time(NULL);
+	struct tm format = *localtime(&now);
+
+	pthread_mutex_lock(&SPRINTF_USE);
+	// Get Time
+	char currentTime[50];
+	memset(currentTime, 0, sizeof(currentTime));
+	sprintf(currentTime, "%d-%02d-%02dT%02d:%02d:%02dU", 
+		format.tm_year + 1900, format.tm_mon + 1, format.tm_mday, format.tm_hour, format.tm_min, format.tm_sec);
+
+	// Make String
+	char temp[1024];
+	sprintf(temp, "%s %s \"%s\" %s %d %d\n", 
+		currentTime, clientIP, request, host, code, size);
+	strcpy(rere, temp);
+	pthread_mutex_unlock(&SPRINTF_USE);
+
+	// Write To file
+	pthread_mutex_lock(&WRITE_ENTRY);
+	FILE* f = fopen("access.log", "a");
+	if (f == NULL){
+		fprintf(stderr, "Error: Unable to read / write file\n");
+		pthread_mutex_unlock(&WRITE_ENTRY);
+		return -1;
+	}
+	fwrite(rere, sizeof(char), strlen(rere), f);
+	fclose(f);
+	pthread_mutex_unlock(&WRITE_ENTRY);
+
+	return 1;
 }
 
 //------------------------------------------------------------------------------------------------------
@@ -95,7 +141,7 @@ struct ServerRes handleGet(int client_fd, int server_fd, char* clientbuffer){
 	char recvbuffer[1024];
 	memset(recvbuffer, '\0' , sizeof(recvbuffer));
 	int inByte = recv(server_fd, recvbuffer, sizeof(recvbuffer), 0);
-	if (inByte < 0){
+	if (inByte <= 0){
 		fprintf(stderr, "ERROR: Recieve failed\n");
 		return rere;
 	}
@@ -106,6 +152,31 @@ struct ServerRes handleGet(int client_fd, int server_fd, char* clientbuffer){
 	strncpy(code_str, recvbuffer + 9, 3);
 	for (int i = 0; code_str[i] != '\0'; ++i){
     	resCode = resCode * 10 + code_str[i] - '0';
+	}
+
+
+	// Remove connection header
+	char* bad = strstr(recvbuffer, "Proxy-Connection: ");
+	if (bad != NULL){
+		char* endbad = strstr(bad, "\r\n");
+		endbad+= 2;
+		char str1[1024];
+		memset(str1, 0, sizeof(str1));
+		strncpy(str1, recvbuffer, bad - &recvbuffer[0]);
+		strcat(str1, endbad);
+		memset(recvbuffer, '\0', sizeof(recvbuffer));
+		strcpy(recvbuffer, str1);
+	}
+	bad = strstr(recvbuffer, "Connection: ");
+	if (bad != NULL){
+		char* endbad = strstr(bad, "\r\n");
+		endbad+= 2;
+		char str1[1024];
+		memset(str1, 0, sizeof(str1));
+		strncpy(str1, recvbuffer, bad - &recvbuffer[0]);
+		strcat(str1, endbad);
+		memset(recvbuffer, '\0', sizeof(recvbuffer));
+		strcpy(recvbuffer, str1);
 	}
 
 	// Determine encoding
@@ -123,6 +194,7 @@ struct ServerRes handleGet(int client_fd, int server_fd, char* clientbuffer){
 		for (int i = 0; size_str[i] != '\0'; ++i){
         	contentLength = contentLength * 10 + size_str[i] - '0';
 		}
+		bytesRead = contentLength;
 		
 		// Countdown from content length from current packet
 		char* startcount = strstr(recvbuffer, "\r\n\r\n");
@@ -133,13 +205,12 @@ struct ServerRes handleGet(int client_fd, int server_fd, char* clientbuffer){
 		startcount += 4;
 
 		if (contentLength > strlen(startcount) || contentLength != 0){
-			if (send(client_fd, recvbuffer, strlen(recvbuffer)-1, 0) <= 0) {
+			if (send(client_fd, recvbuffer, inByte, 0) <= 0) {
 				fprintf(stderr, "ERROR: Send to client failed\n");
 				return rere;
 			}
 
 			contentLength -= strlen(startcount);
-			bytesRead += inByte;
 		}
 		else{
 			char temp[1024];
@@ -157,25 +228,23 @@ struct ServerRes handleGet(int client_fd, int server_fd, char* clientbuffer){
 
 		// Get rest of packets
 		while(1){
-			memset(recvbuffer, 0, sizeof(recvbuffer));
+			memset(recvbuffer, '\0', sizeof(recvbuffer));
 			inByte = recv(server_fd, recvbuffer, sizeof(recvbuffer), 0);
-			if (inByte < 0){
+			if (inByte <= 0){
 				fprintf(stderr, "ERROR: Recieve failed\n");
 				return rere;
 			}
 
 			if (contentLength > inByte){
-				bytesRead += inByte;
 				contentLength -= inByte;
-				if (send(client_fd, recvbuffer, strlen(recvbuffer), 0) <= 0) {
+				if (send(client_fd, recvbuffer, inByte, 0) <= 0) {
 					fprintf(stderr, "ERROR: Send to client failed\n");
 					return rere;
 				}
 
 			}
 			else{
-				bytesRead += contentLength;
-				if (send(client_fd, recvbuffer, strlen(recvbuffer), 0) <= 0) {
+				if (send(client_fd, recvbuffer, inByte, 0) <= 0) {
 					fprintf(stderr, "ERROR: Send to client failed\n");
 					return rere;
 				}
@@ -189,15 +258,80 @@ struct ServerRes handleGet(int client_fd, int server_fd, char* clientbuffer){
 	// Deal with chunked encoding
 	getTypeCL = strstr(recvbuffer, "Transfer-Encoding: chunked");
 	if (getTypeCL != NULL){
+		//get Chunk
+		char* getChunk = strstr(recvbuffer, "\r\n\r\n") + 4;
+		char* endl = NULL;
+		int currentChunk = strtol(getChunk, &endl, 16);
+		bytesRead = currentChunk;
 		
+		// Send First
+		recvbuffer[inByte] = '\0';
+		if (send(client_fd, recvbuffer, inByte, 0) <= 0) {
+			fprintf(stderr, "ERROR: Send to client failed\n");
+			return rere;
+		}
+		currentChunk -= inByte;
 
+		// Read in chunks
+		while(1){
+			memset(recvbuffer, 0, sizeof(recvbuffer));
+			inByte = recv(server_fd, recvbuffer, sizeof(recvbuffer), 0);
+			if (inByte <= 0){
+				fprintf(stderr, "ERROR: Recieve failed\n");
+				return rere;
+			}
 
+			if (strstr(recvbuffer, "0\r\n\r\n") != NULL){
+				if (send(client_fd, recvbuffer, strlen(recvbuffer), 0) <= 0) {
+					fprintf(stderr, "ERROR: Send to client failed\n");
+					return rere;
+				}
+				rere.code = resCode;
+				rere.bytes = bytesRead;
+				return rere;
+			}
+
+			// Check if new chunk
+			if (currentChunk <= 0){
+				currentChunk = strtol(recvbuffer, &endl, 16);
+				bytesRead += currentChunk;
+			}
+
+			// Send chunk
+			recvbuffer[inByte] = '\0';
+			if (send(client_fd, recvbuffer, strlen(recvbuffer), 0) <= 0) {
+				fprintf(stderr, "ERROR: Send to client failed\n");
+				return rere;
+			}
+			currentChunk -= inByte;
+		}		
 	}
 
 	fprintf(stderr, "ERROR: Only support chunked encoding or content length\n");
 	return rere;
 }
 
+struct ServerRes handleConnect(int client_fd, int server_fd, char* clientbuffer){
+	struct ServerRes rere;
+	rere.bytes = -1;
+	rere.code = -1;
+	if (send(server_fd, clientbuffer, strlen(clientbuffer), 0) <= 0) {
+		fprintf(stderr, "ERROR: Send to host server failed\n");
+	}
+	printf ("%s\n\n\n", clientbuffer);
+	
+	char recvbuffer[1024];
+	while(1){
+		memset(recvbuffer, 0, sizeof(recvbuffer));
+		int inByte = recv(server_fd, recvbuffer, sizeof(recvbuffer), 0);
+		if (inByte <= 0){
+			fprintf(stderr, "ERROR: Recieve failed\n");	
+			break;	
+			}
+		printf("---------\n%s\n\n\n",recvbuffer);
+	}
+	return rere;
+}
 
 // Multithread function---------------------------------------------------------------------------------
 // Handle Client's request
@@ -209,37 +343,30 @@ void* handleRequest(void* fd){
 
 	// Recieve Data from client
 	int recvReq = recv(sock, buffer, sizeof(buffer), 0);
-	if (recvReq < 0){
+	if (recvReq <= 0){
 		fprintf(stderr, "Error: Recv Failed from client\n");
 		close(sock);
 		return NULL;
 	}
 
-	// Get Time
-	char currentTime[1024];
-	time_t now = time(NULL);
-	struct tm format = *localtime(&now);
-	sprintf(currentTime, "%d-%02d-%02dT%02d:%02d:%02d", 
-		format.tm_year + 1900, format.tm_mon + 1, format.tm_mday, format.tm_hour, format.tm_min, format.tm_sec);
-
 	// Set to -1 if request format is wrong
 	// Set to -2 if unimplemented command
 	// Set to -3 if forbidden site
-	char status = 0;
+	int status = 0;
 
 	// Get RequestLine
 	char requestLine[1024];
+	memset(requestLine, 0, sizeof(requestLine));
 	char* endline = strchr(buffer, '\r');
 	strncpy(requestLine, buffer, endline-buffer);
 
 	// Get Host
 	char hostAr[256];
 	memset(hostAr, 0, sizeof(hostAr));
-	char* host;
-	host = strstr(buffer, "Host: ");
+	char* host = strstr(buffer, "Host: ");
 	if (host == NULL) {
 		strcpy(hostAr, "");
-		status =  -1;
+		status = -1;
 	}
 	else {
 		host += 6;
@@ -268,17 +395,40 @@ void* handleRequest(void* fd){
 	}
 	else if (siteAllowed == 0) if (status == 0) status = -3;
 
+	// Remove Connection Header
+	char* bad = strstr(buffer, "Proxy-Connection: ");
+	if (bad != NULL){
+		char* endbad = strstr(bad, "\r\n");
+		endbad+= 2;
+		char str1[1024];
+		memset(str1, 0, sizeof(str1));
+		strncpy(str1, buffer, bad - &buffer[0]);
+		strcat(str1, endbad);
+		memset(buffer, 0, sizeof(buffer));
+		strcpy(buffer, str1);
+	}
+	bad = strstr(buffer, "Connection: ");
+	if (bad != NULL){
+		char* endbad = strstr(bad, "\r\n");
+		endbad+= 2;
+		char str1[1024];
+		memset(str1, 0, sizeof(str1));
+		strncpy(str1, buffer, bad - &buffer[0]);
+		strcat(str1, endbad);
+		memset(buffer, 0, sizeof(buffer));
+		strcpy(buffer, str1);
+	}
+
 	//-----------------------------------------------------------------------------
+	// Get Host
+	struct hostent* hostData= gethostbyname_ts(hostAr);
+	if(hostData == NULL){
+		fprintf(stderr, "Error: gethostbyname failed to return successfully\n");
+		status = -1;
+	}
 
 	// Do Request if valid
-	if ((memcmp("GET ", buffer, 4) == 0 || memcmp("CONNECT ", buffer, 8) == 0) && status == 0){
-		struct hostent* hostData= gethostbyname_ts(hostAr);
-		if(hostData == NULL){
-			fprintf(stderr, "Error: gethostbyname failed to return successfully\n");
-			close(sock);
-			return NULL;
-		}
-
+	if (status == 0){
 		// Get Server IP
 		char serverIP[256];
 		struct in_addr **addr_list = (struct in_addr **) hostData->h_addr_list;
@@ -314,8 +464,7 @@ void* handleRequest(void* fd){
 			return NULL;
 		}
 
-
-		// Do something
+		// Do something First time
 		if (memcmp("GET ", buffer, 4) == 0){
 			struct ServerRes get = handleGet(sock, host_fd, buffer);
 			if (get.code == -1 && get.bytes == -1){
@@ -323,20 +472,153 @@ void* handleRequest(void* fd){
 				close(host_fd);
 				return NULL;
 			}
+
+			// Log Entry
+			logEntry(tdata->cIP, requestLine, hostAr, get.code, get.bytes);
 		}
 		else if (memcmp("CONNECT ", buffer, 8) == 0){
+			struct ServerRes con = handleConnect(sock, host_fd, buffer); 
+			if (con.code == -1 && con.bytes == -1){
+				close(sock);
+				close(host_fd);
+				return NULL;
+			}
 
+			// Log Entry
+			logEntry(tdata->cIP, requestLine, hostAr, con.code, con.bytes);
 		}
+		// Unimplemented Method
+		else{
+			char* methodRejected = "HTTP/1.1 405 Method Not Allowed\r\nContent-Length: 0\r\n\r\n";
+            if (send(sock, methodRejected, strlen(methodRejected), 0) <= 0) {
+				fprintf(stderr, "ERROR: Send to host server failed\n");
+				close(sock);
+				close(host_fd);
+				return NULL;
+			}
+
+			// Log Entry
+			logEntry(tdata->cIP, requestLine, hostAr, 405, 0);
+		}
+
+
+		// Maintain Connections
+		int res = 0;
+		fd_set list;
+		FD_ZERO(&list);
+		FD_SET(sock, &list);
+		FD_SET(host_fd, &list);
+
+		// Check for Sent data
+    	while(res >=0){
+    		res = input_timeout(list, 300);
+    		if (res > 0){
+    			if (FD_ISSET(sock, &list)){
+    				memset (buffer, 0, sizeof(buffer));
+				 	recvReq = recv(sock, buffer, sizeof(buffer), 0);
+					if (recvReq <= 0){
+						if (recvReq < 0) fprintf(stderr, "Error: Send Failed from client x2\n");
+						close(host_fd);
+						close(sock);
+						return NULL;
+					}
+					// Get RequestLine
+					memset(requestLine, 0, sizeof(requestLine));
+					endline = strchr(buffer, '\r');
+					strncpy(requestLine, buffer, endline-buffer);
+
+					// DO STUFF Repeat
+					// Check Request CONN
+					if (memcmp("GET ", buffer, 4) == 0){
+						struct ServerRes get = handleGet(sock, host_fd, buffer);
+						if (get.code == -1 && get.bytes == -1){
+							close(sock);
+							close(host_fd);
+							return NULL;
+						}
+
+						// Log Entry
+						logEntry(tdata->cIP, requestLine, hostAr, get.code, get.bytes);
+					}
+					else if (memcmp("CONNECT ", buffer, 8) == 0){
+						struct ServerRes con = handleConnect(sock, host_fd, buffer); 
+						if (con.code == -1 && con.bytes == -1){
+							close(sock);
+							close(host_fd);
+							return NULL;
+						}
+
+						// Log Entry
+						logEntry(tdata->cIP, requestLine, hostAr, con.code, con.bytes);
+					}
+					// Unimplemented Method
+					else{
+						char* methodRejected = "HTTP/1.1 405 Method Not Allowed\r\nContent-Length: 0\r\n\r\n";
+			            if (send(sock, methodRejected, strlen(methodRejected), 0) <= 0) {
+							fprintf(stderr, "ERROR: Send to host server failed\n");
+							close(sock);
+							close(host_fd);
+							return NULL;
+						}
+
+						// Log Entry
+						logEntry(tdata->cIP, requestLine, hostAr, 405, 0);
+					} 
+					
+    			}
+    			else{
+    				memset (buffer, 0, sizeof(buffer));
+				 	recvReq = recv(host_fd, buffer, sizeof(buffer), 0);
+
+				 	// Server initiated Shutdown
+				 	if (recvReq <= 0){
+						close(host_fd);
+						close(sock);
+						return NULL;
+					}
+    			}
+    		}
+    		// Timeout
+    		else{
+				fprintf(stderr, "Error: Client / Server Timout\n");
+				close(host_fd);
+				close(sock);
+				return NULL;
+    		}
+    	}
+		
 
 		close(host_fd);
 	}
 
 	// Send error response to client
 	else {
-		if(status == 0) status = -2;
+		char* methodRejected = NULL;
+		int badCode = 0;
+		if (status == -1){
+			methodRejected = "HTTP/1.1 400 Bad Request\r\nContent-Length: 0\r\n\r\n";
+			badCode = 400;
+		}
+		if (status == -3){
+			methodRejected = "HTTP/1.1 403 Forbidden\r\nContent-Length: 0\r\n\r\n";
+			badCode = 403;
+		}
+		else {
+			fprintf(stderr, "ERROR: Unknown status: %d\n", status);
+			close(sock);
+			return NULL;
+		}
 
+		// Send back to client
+        if (send(sock, methodRejected, strlen(methodRejected), 0) <= 0) {
+			fprintf(stderr, "ERROR: Send to client failed\n");
+			close(sock);
+			return NULL;
+		}
+
+		// Log Entry
+		logEntry(tdata->cIP, requestLine, hostAr, badCode, 0);
 	}
-
 
 	close(sock);
 	return NULL;
@@ -349,6 +631,14 @@ void* handleRequest(void* fd){
 // Main Server function ----------------------------------------------------------------------------------
 int main(int argc, char **argv){
 	if (pthread_mutex_init(&GET_HOST, NULL) != 0){
+		fprintf(stderr, "Error: Failed to Init mutex");
+    	exit(1);
+	}
+	if (pthread_mutex_init(&WRITE_ENTRY, NULL) != 0){
+		fprintf(stderr, "Error: Failed to Init mutex");
+    	exit(1);
+	}
+	if (pthread_mutex_init(&SPRINTF_USE, NULL) != 0){
 		fprintf(stderr, "Error: Failed to Init mutex");
     	exit(1);
 	}
@@ -439,9 +729,11 @@ int main(int argc, char **argv){
 			}
 			sleep(1);
 		}
-		close(comm_fd);
+		//close(comm_fd);
 	}
 
 	close(listen_fd);
 	pthread_mutex_destroy (&GET_HOST);
+	pthread_mutex_destroy (&WRITE_ENTRY);
+	pthread_mutex_destroy (&SPRINTF_USE);
 }
